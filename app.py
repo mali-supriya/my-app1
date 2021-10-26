ac59e60e0d204526539f76201377c75a7a31f7f3
mali-supriya <55476570+mali-supriya@users.noreply.github.com>
2021-10-26 12:40:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ app = Flask(__name__)
 @app.route('/api',methods=['POST'])
 def index():
     record = json.loads(request.data)
-    schedule_id=record["schedule_id"]
-    TOKEN=record["X-AUTH-TOKEN"]
-    headers = {"X-AUTH-TOKEN": TOKEN}
-    callback_url_input = record["callback_url_input"]
-    response = requests.get(callback_url_input,headers=headers)
-    print(response.json())
-    callback_url_output = record["callback_url_output"]
+    #schedule_id=record["schedule_id"]
+    #TOKEN=record["X-AUTH-TOKEN"]
+    #headers = {"X-AUTH-TOKEN": TOKEN}
+    #callback_url_input = record["callback_url_input"]
+    #response = requests.get(callback_url_input,headers=headers)
+    #print(response.json())
+    #callback_url_output = record["callback_url_output"]
     #headers['Content-Type']='application/json'
     #json_path = r"outputjson.json"
     #f = open(json_path,)
