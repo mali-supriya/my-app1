cd833d2157c7c3b1787e91b1ac568de33e74461a
mali-supriya <55476570+mali-supriya@users.noreply.github.com>
2021-10-27 19:31:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,27 +2,32 @@ import json
 import pyscipopt
 import requests
 from flask import Flask, request, jsonify
+from mip_optimization import Optimization
 app = Flask(__name__)
 
 @app.route('/api',methods=['POST'])
 def index():
-    record = json.loads(request.data)
-    schedule_id=record["schedule_id"]
-    TOKEN=record["X-AUTH-TOKEN"]
-    headers = {"X-AUTH-TOKEN": TOKEN}
-    callback_url_input = record["callback_url_input"]
-    callback_url_input = callback_url_input+"?id="+str(schedule_id)
-    response = requests.get(callback_url_input,headers=headers)
-    print(response.json())
-    callback_url_output = record["callback_url_output"]
-    headers['Content-Type']='application/json'
-    json_path = r"outputjson.json"
-    f = open(json_path,)
-    data = json.load(f)
-    data = json.dumps(data)
-    response = requests.post(callback_url_output, data=data, headers=headers)
-    print(response.text)
-    return "Success"
-
+    try:
+        record = json.loads(request.data)
+        schedule_id=record["schedule_id"]
+        TOKEN=record["X-AUTH-TOKEN"]
+        headers = {"X-AUTH-TOKEN": TOKEN}
+        callback_url_input = record["callback_url_input"]
+        callback_url_input = callback_url_input+"?id="+str(schedule_id)
+        response = requests.get(callback_url_input,headers=headers)
+        data=response.json()
+        sch_hist = len(data['scheduleData']["schedule_history"])
+        Optimization(data,sch_hist)
+        callback_url_output = record["callback_url_output"]
+        headers['Content-Type']='application/json'
+        json_path = r"output.json"
+        f = open(json_path,)
+        data = json.load(f)
+        output = json.dumps(data)
+        response = requests.post(callback_url_output, data=output, headers=headers)
+        print(response.text)
+        return 'success', 200
+    except:
+        'bad request!', 400
 if __name__ == '__main__':
     app.run()
